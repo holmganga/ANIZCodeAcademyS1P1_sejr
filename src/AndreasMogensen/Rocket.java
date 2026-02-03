020d6089c95e28c3b3b6e1fdbc0fe256df4affa8
holmganga <siggi.sejr@gmail.com>
2026-02-03 09:50:32 +0100

    andreasmogensen

---
+++ src/AndreasMogensen/Rocket.java
@@ -0,0 +1,19 @@
+package AndreasMogensen;
+
+public class Rocket {
+    public static void main (String[] args){
+        System.out.println("      /\\");
+        System.out.println("     /  \\");
+        System.out.println("    /____\\");
+        System.out.println("    |    |");
+        System.out.println("    |ISS |");
+        System.out.println("    |    |");
+        System.out.println("    |____|");
+        System.out.println("      ||");
+        System.out.println("      ||");
+        System.out.println("      ||");
+        System.out.println("     /  \\");
+        System.out.println("    /____\\");
+        System.out.println("     /\\/\\");
+    }
+}
