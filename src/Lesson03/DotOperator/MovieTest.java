bfc8f18f3e96ee56ba31417166272be4cbb961ae
holmganga <siggi.sejr@gmail.com>
2026-02-03 11:34:59 +0100

    exercises 03/02

--- src/Lesson03/DotOperator/MovieTest.java
+++ src/Lesson03/DotOperator/MovieTest.java
@@ -7,18 +7,28 @@ public class MovieTest {
         m1.title = "Inception";
         m1.genre = "Sci-Fi";
         m1.rating = 9;
+        m1.year = 2010;
         m1.playIt();
 
         Movie m2 = new Movie();
         m2.title = "The Dark Knight";
         m2.genre = "Action";
         m2.rating = 10;
+        m2.year = 2008;
         m2.playIt();
 
         Movie m3 = new Movie();
         m3.title = "Frozen";
         m3.genre = "Animation";
         m3.rating = 8;
+        m3.year = 2013;
         m3.playIt();
+
+        Movie m4 = new Movie();
+        m4.title = "Sinners";
+        m4.genre = "Horror";
+        m4.rating = 10;
+        m4.year = 2025;
+        m4.playIt();
     }
 }
--- src/Lesson03/DotOperator/PersonTest.java
+++ src/Lesson03/DotOperator/PersonTest.java
@@ -8,12 +8,21 @@ public class PersonTest {
         String mName = "Elias";
         String lName = "Mogensen";
         int age = 47;
+        String prof = "Astronaut";
 
-        OpretNytPersonObjekt(fName, mName, lName, age);
+        OpretNytPersonObjekt(fName, mName, lName, age, prof);
+
+        String fName2 = "Sejr";
+        String mName2 = "Staugaard";
+        String lName2 = "Thomsen";
+        int age2 = 29;
+        String prof2 = "Studerende";
+
+        OpretNytPersonObjekt (fName2, mName2, lName2, age2, prof2); //hvordan genkender metoden disse variabler?
 
     }
 
-    public static void OpretNytPersonObjekt(String fName, String mName, String lName, int age)
+    public static void OpretNytPersonObjekt(String fName, String mName, String lName, int age, String prof)
     {
 
         // Opretter et nyt Person-objekt
@@ -24,6 +33,7 @@ public class PersonTest {
         p.middleName = mName;
         p.lastName = lName;
         p.age = age;
+        p.profession = prof;
 
         //Udskriv
         p.WritePersonInfo();
