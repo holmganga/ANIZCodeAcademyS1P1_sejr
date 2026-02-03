8dd3ffdbf28fbbbc88fb225988c0784d322e187b
holmganga <siggi.sejr@gmail.com>
2026-02-03 09:50:32 +0100

    andreasmogensen

---
+++ src/Lesson01/PrintOrPrintln/NameTag.java
@@ -0,0 +1,12 @@
+package Lesson01.PrintOrPrintln;
+
+public class NameTag {
+    public static void main (String[] args){
+        System.out.print(nameTag());
+    }
+
+    public static String nameTag(){
+        String tag = "***********************\n*        SETH         *\n* seth1000@stud.ek.dk *\n***********************";
+        return tag;
+    }
+}
