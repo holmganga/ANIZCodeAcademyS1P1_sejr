7bcb5724e2513c67aa6ad0abcbb738e76dea0526
holmganga <siggi.sejr@gmail.com>
2026-02-03 09:50:32 +0100

    andreasmogensen

---
+++ src/Lesson01/PrintOrPrintln/AdvancedExercise.java
@@ -0,0 +1,13 @@
+package Lesson01.PrintOrPrintln;
+
+
+public class AdvancedExercise {
+    public static void main(String[] args) {
+        System.out.println(output());
+    }
+    public static String output(){
+        String text = "Hello, world! This is on the same line. Still the same line! Now we go to a new line\n" +
+                "Cake is ready\n";
+        return text;
+    }
+}
--- src/Lesson01/PrintOrPrintln/WriteToNewLine02.java
+++ src/Lesson01/PrintOrPrintln/WriteToNewLine02.java
@@ -2,12 +2,13 @@ package Lesson01.PrintOrPrintln;
 
 public class WriteToNewLine02 {
 
-
+    //skal ændres til "Hej Sofie! Du er 20 år gammel, 10 år yngre end mig. Velkommen til Java."
     public static void main(String[] args) {
         String navn = "Sofie";
         int alder = 20;
+        int minAlder = 10;
 
-        System.out.println("Hej " + navn + "! Du er " + alder + " år gammel.");
+        System.out.println("Hej " + navn + "! Du er " + alder + " år gammel " + minAlder + " år yngre end mig.");
         System.out.println("Velkommen til Java 🚀");
     }
 }
