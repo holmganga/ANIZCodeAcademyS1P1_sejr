5ccf906d61f27574ac8c92bc0df29ef3913054ea
holmganga <siggi.sejr@gmail.com>
2026-02-03 11:34:59 +0100

    exercises 03/02

--- src/Lesson03/DotOperator/Person.java
+++ src/Lesson03/DotOperator/Person.java
@@ -6,11 +6,13 @@ public class Person {
     String lastName;
     String middleName;
     int age;
+    String profession;
 
     public void WritePersonInfo()
     {
         // Udskriver oplysninger
         System.out.println("Navn: " + firstName + " " + middleName + " " + lastName);
         System.out.println("Alder: " + age);
+        System.out.println(profession);
     }
 }
