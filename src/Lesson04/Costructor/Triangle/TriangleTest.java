29a65c4f073bf5d08ef6bdd1256c9997d0d50b20
holmganga <siggi.sejr@gmail.com>
2026-02-04 11:35:00 +0100

    triangle exercise

--- src/Lesson04/Costructor/Triangle/TriangleTest.java
+++ src/Lesson04/Costructor/Triangle/TriangleTest.java
@@ -5,9 +5,21 @@ public class TriangleTest {
         Triangle t1 = new Triangle(3, 4, 5);
         t1.showInfo();
 
-        System.out.println();
-
         Triangle t2 = new Triangle(6, 6, 6);
         t2.showInfo();
+
+        Triangle t3 = new Triangle(10,12,9);
+        t3.showInfo();
+
+        if (t1.getArea() > t2.getArea() && t1.getArea() > t3.getArea()){
+            System.out.print("Triangle 1 is largest.");
+        }else if (t2.getArea() > t1.getArea() && t2.getArea() > t3.getArea()){
+            System.out.print("Triangle 2 is largest.");
+        }else if (t3.getArea() > t2.getArea() && t3.getArea() > t1.getArea()){
+            System.out.print("Triangle 3 is largest.");
+        }else {
+            System.out.print("error");
+        }
+
+        }
     }
-}
